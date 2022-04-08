4d8f540bf189033b6fe069824b506dc13110e82c
Matheus O Silva <oliveiramatheus709@gmail.com>
2022-04-07 23:28:31 -0300

    first commit

---
+++ esfera.js
@@ -0,0 +1,13 @@
+const PI = 3.14159;
+
+let R = parseFloat(gets());
+
+
+
+//TODO: Complete os espaços em branco com uma possível solução para o desafio
+
+var volumeEsfera = (4/3) * PI * Math.pow(R, 3);
+
+
+
+print("VOLUME =    " + volumeEsfera.toFixed(3));
